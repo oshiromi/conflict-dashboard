78bce2d62bea2ae843a9a90845ef2d59954e3da6
Kristian André Jakobsen <k.andrejakobsen@gmail.com>
2022-01-22 22:51:20 +0100

    Started the app and imported data

---
+++ src/streamlit_app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+header = st.container()
+body = st.container()
+col1, col2 = st.columns(2)
+url = 'https://ucdp.uu.se/downloads/ucdpprio/ucdp-prio-acd-211-csv.zip'
+
+
+country = st.sidebar.selectbox(
+    'Select a country',
+    ('First', 'Second', 'Third')
+)
+
+with header:
+    st.title('Visualizing natural disasters')
+
+with body:
+    st.header(f'Map for {country}')
+    st.text('Here there is going to be a map.')
+    df = pd.read_csv(url, compression='zip')
+    st.write(df)
+    with col1:
+        st.write('Hey!')
+    with col2:
+        st.write('No')

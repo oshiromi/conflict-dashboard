028dccc45e8b8cea14824c897bfa4db0119f0e74
oshiromi <85414986+oshiromi@users.noreply.github.com>
2022-01-29 19:05:11 +0100

    data exploration and plots

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,11 @@
-type_of_violence = {1: 'State-based armed conflict',
+TYPE_OF_VIOLENCE = {1: 'State-based armed conflict',
                     2: 'Non-state conflict',
                     3: 'One-sided violence'}
+
+COLUMNS = ['id', 'year', 'active_year', 
+    'type_of_violence', 'conflict_name', 'side_a', 
+    'side_b', 'where_prec', 'where_coordinates', 
+    'latitude', 'longitude', 'country',
+    'region', 'date_prec', 'date_start', 
+    'date_end', 'deaths_a', 'deaths_b', 
+    'deaths_civilians', 'deaths_unknown']
--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from constants import COLUMNS
 
 def load_conflict_data():
     url = 'https://ucdp.uu.se/downloads/ged/ged211-csv.zip'
     conflict_df = pd.read_csv(url, compression='zip')
+    conflict_df = conflict_df[COLUMNS]
     conflict_df['date_start'] = pd.to_datetime(conflict_df['date_start'])
     conflict_df['date_end'] = pd.to_datetime(conflict_df['date_end'])
     conflict_df = conflict_df.set_index('date_start', drop = True).sort_index()
     return conflict_df
+    

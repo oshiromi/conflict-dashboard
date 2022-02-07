d684f7fa1d635add79d40f1a86da9381d910e1a1
Kristian André Jakobsen <k.andrejakobsen@gmail.com>
2022-02-07 10:40:15 +0100

    Added data types

--- src/constants.py
+++ src/constants.py
@@ -9,3 +9,11 @@ COLUMNS = ['id', 'year', 'active_year',
     'region', 'date_prec', 'date_start', 
     'date_end', 'deaths_a', 'deaths_b', 
     'deaths_civilians', 'deaths_unknown', 'best']
+
+DTYPES = {'id': int, 'year': int, 'active_year': int, 
+    'type_of_violence': int, 'conflict_name': str, 'side_a': str, 
+    'side_b': str, 'where_prec': int, 'where_coordinates': str, 
+    'latitude': float, 'longitude': float, 'country': str,
+    'region': str, 'date_prec': int, 'date_start': str, 
+    'date_end': str, 'deaths_a': int, 'deaths_b': int, 
+    'deaths_civilians': int, 'deaths_unknown': int, 'best': int}
